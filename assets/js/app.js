f0ee0bd3ad35fae34c56a66a8dbebc98e385a75a
M. Appelman <huphtur@gmail.com>
2018-04-02 20:53:50 -0700

    Removed jQuery and Popper.js dependencies
    and load from CDN instead, because HTTP/2 is much better.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,12 @@
 // Initialize Bootstrap
 // --------------------------------------------------
 
+// Enable Tooltips and popovers for displaying and positioning
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+// Enable Dropdowns for displaying and positioning
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
